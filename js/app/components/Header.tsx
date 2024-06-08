1db129f0c9a17c4bdebebee7ef6c1c538d41ccb5
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-06-08 16:45:34 -0700

    Header bar improvement

--- js/app/components/Header.tsx
+++ js/app/components/Header.tsx
@@ -2,7 +2,7 @@ import UserIcon from "./UserIcon";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-5">
+    <header className="flex justify-between items-center p-5 fixed w-full backdrop-blur border-b dark:border-gray-600">
       <h1 className="text-5xl">Studier</h1>
       <div className="flex items-center">
         <button className="bg-green-600 hover:bg-green-800 text-white hover:text-gray-300 text-xl p-4 rounded-md mr-5">+ Post</button>
