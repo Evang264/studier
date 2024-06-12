d7864fa601bf357635143dc909a827e1926afc8f
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-06-12 13:56:42 -0700

    Blur background for ModalComponent

--- js/app/components/ModalComponent.tsx
+++ js/app/components/ModalComponent.tsx
@@ -12,7 +12,11 @@ export default function ModalComponent({show, onClose, children}: {
     return;
 
   return (
-    <div className="fixed inset-0 bg-blur flex items-center justify-center z-50">
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm" onClick={() => {
+          onClose();
+          setVisible(false);
+      }}></div>
       <div className="bg-zinc-700 p-8 rounded shadow-lg relative">
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
