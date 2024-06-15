4831229374ae350c69bc4ea8e5105f6f8991aa13
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-06-15 13:56:44 -0700

    Allow clicking on Post's user profile

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
-import { fetchUser, fetchPost, updatePost } from "@/lib/database";
-import LoadingIndicator from "./LoadingIndicator";
+import { updatePost } from "@/lib/database";
 import { IUser, IPost } from "@/lib/database";
 import ProtectedInput from "./ProtectedInput";
 
@@ -20,38 +19,35 @@ export default function Post({
   return (
     <div>
       <div className="flex flex-row mb-2 w-full items-center">
-        <Image
-          src={user.pfp}
-          width={45}
-          height={45}
-          alt={`${user.name}'s profile picture`}
-          className="mr-4"
-        />
-        <div>
-          <p>
-            <b>{user.name}</b>
-          </p>
-          <p>
-            <i>{user.bio}</i>
-          </p>
-        </div>
+        <Link
+          href={`/user/${post.userId}`}
+          className="flex flex-row items-center"
+        >
+          <Image
+            src={user.pfp}
+            width={45}
+            height={45}
+            alt={`${user.name}'s profile picture`}
+            className="mr-4"
+          />
+          <div>
+            <p className="font-bold">{user.name}</p>
+            <p className="italic">{user.bio}</p>
+          </div>
+        </Link>
       </div>
       <hr className="my-4" />
       <ProtectedInput
         text={post.title}
         editable={editable}
         className="text-2xl mb-2"
-        {...(editable? {
-          onUpdate: (text: string) => updatePost(postId!, { title: text })
-        }: {})}
+        onUpdate={(text: string) => updatePost(postId!, { title: text })}
       />
       <ProtectedInput
         text={post.description}
         editable={editable}
         multiline
-        {...(editable? {
-          onUpdate: (text: string) => updatePost(postId!, { description: text })
-        }: {})}
+        onUpdate={(text: string) => updatePost(postId!, { description: text })}
       />
     </div>
   );
