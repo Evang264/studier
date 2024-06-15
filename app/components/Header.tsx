3f5bc64da7ccfd97c00df7d0fcacd69a1a32869a
Evan Zhao <57083990+Evang264@users.noreply.github.com>
2024-06-15 14:43:39 -0700

    Allow header bar to be clickable

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 import UserIcon from "./UserIcon";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center p-5 sticky w-full backdrop-blur border-b dark:border-gray-600">
-      <h1 className="text-5xl">Studier</h1>
+      <Link href="/" className="text-5xl">Studier</Link>
       <div className="flex items-center">
         <button className="bg-green-600 hover:bg-green-800 text-white hover:text-gray-300 text-xl p-4 rounded-md mr-5">+ Post</button>
         
